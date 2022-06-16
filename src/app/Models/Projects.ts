2a7ceea702e2ae8c7a253bb1981b74f490df136b
BOBOLA <w0772193@myscc.ca>
2022-06-16 17:18:44 -0400

    included proper project links

--- src/app/Models/Projects.ts
+++ src/app/Models/Projects.ts
@@ -3,11 +3,13 @@ export class Projects{
   name: string
   manager: string
   description: string
+  links: String
 
-  constructor(image: string, name: string, manager: string, description: string) {
+  constructor(image: string, name: string, manager: string, description: string, links: string) {
     this.image = image;
     this.name = name;
     this.manager = manager;
     this.description = description;
+    this.links = links;
   }
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,18 +12,21 @@ export class ProjectsComponent implements OnInit {
   constructor() {
     this.projects = [
       {
-        image: "https://www.enactusstclair.ca/interAct/images/interACT-sm.png",
+        links: "https://www.enactusstclair.ca/interAct/",
+         image: "https://www.enactusstclair.ca/interAct/images/interACT-sm.png",
         name: "interACT",
         manager: "Project Manager: Bethany D’Alimonte – interactwindsor@gmail.com",
         description: "interACT is a financial literacy project that aims to support the livelihood of international students as they pursue their studies and look to establish themselves in Canada’s work culture. Our team directly supports these students through the development of our live mobile application and website, as well as through mock interviews, seminars, and resume clinics.",
       },
       {
+        links: "",
         image:"https://www.enactusstclair.ca/wp-content/uploads/2022/01/reVERT-300x180.png",
         name:"reVERT",
         manager:"Project Manager – Trevor Ramieri – trevor.ramieri01@stclairconnect.ca",
         description:"reVERT is a green tech sustainability project that looks to divert unnecessary waste from landfills from institutions. We do this by working with waste management teams to better assess their consumers’ waste habits and generate real-time solutions. This is done through direct education and our developed solutions, such as the development of our waste sensor system"
       },
       {
+        links: "",
         image:"https://www.enactusstclair.ca/wp-content/uploads/2022/01/E.A.S.T._-_Dark-300x300.png",
         name:"E.A.S.T",
         manager:"Project Manager: Blake Paterson – BP26@myscc.ca",
@@ -32,6 +35,7 @@ export class ProjectsComponent implements OnInit {
           "    We support participants in multiple stages of their development, such as through our six-week workbook and \n" +
           "    vending opportunities. Our project supports the efforts of these businesses while fostering a true community."
       }, {
+        links: "",
         image:"https://www.enactusstclair.ca/wp-content/uploads/2022/01/Endure-Dark-300x300.png",
         name:"Endure",
         manager:"Project Manager: Francesco DiLeonardo – fd@myscc.ca",
@@ -42,6 +46,7 @@ export class ProjectsComponent implements OnInit {
           "    our project instills workplace confidence and personal development and promotes an inclusive campus community."
       },
       {
+        links: "",
         image:"https://www.enactusstclair.ca/wp-content/uploads/2022/01/transPLANT-300x100.png",
         name:"transPLANT",
         manager:"Project Manager: Brianna Gaylord – BG39@myscc.ca",
@@ -51,6 +56,7 @@ export class ProjectsComponent implements OnInit {
           "    more effective cost savings in areas such as plant replacement and utilities."
       },
       {
+        links: "",
         image:"https://www.enactusstclair.ca/wp-content/uploads/2022/01/palma-300x300.png",
         name:"Palma",
         manager:"Project Manager: Sierra Piekosz – SP215@myscc.ca",
